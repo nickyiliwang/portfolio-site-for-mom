cfe520db96769420d68ec89f5e5c7332d1c3e727
Nick Wang <nickyiliwang@gmail.com>
2020-05-04 18:15:14 -0400

    loading page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Portfolio site for mom
+
+## Goals
+
+Has login and signUp
+
+1. user profile with upload function, user can upload their artwork and have an year made, description, and title.
+
+2. Home page displays All users, and you can click inside to see all their work.
--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const UploadPage = () => (
-  <div>
-    <h1>UploadPage</h1>
-  </div>
-);
+const UploadPage = (props) => {
+  // useEffect will call and get a list of mom's work
+
+  return (
+    <section className="upload-page">
+      <h2>Please Upload your work here</h2>
+      <form>
+        <label for="submit">
+          <input name="submit" type="submit" />
+        </label>
+      </form>
+
+      <div>Here is a list of all your artwork!</div>
+      <div className="artwork-container"></div>
+    </section>
+  );
+};
 
 export default UploadPage;
